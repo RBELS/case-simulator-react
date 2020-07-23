90dc6b96b9dae4038982205fbecb3d4d13597c66
Artyom Belsky <artyombel2003@gmail.com>
2020-07-23 17:10:05 +0300

    Content markup was done. Case item ready.

---
+++ src/components/Content/Case/Case.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Grid, Typography, Card, CardContent, CardActions, Button, CardMedia, CardActionArea } from '@material-ui/core';
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(theme => ({
+    case: {
+        border: "1px solid black",
+        width: "100%",
+        height: "100%",
+    },
+    caseContainer: {
+        // backgroundColor: "beige",
+        width: "100%",
+        // height: 300,
+        display: "inline-block",
+        padding: 20
+    },
+    media: {
+        height: 125,
+        width:150,
+        borderRadius:10,
+        backgroundColor: "lightgrey"
+    },
+    area: {
+        display: "flex",
+        flexDirection: "column"
+    },
+    bt: {
+        backgroundColor: "#F3F3F3",
+        "&:hover": {
+            backgroundColor: "#E5E5E5"
+        }
+    }
+    
+}));
+
+const Case = () => {
+    const classes = useStyles();
+
+    return <Grid className={classes.caseContainer} item xs={12} sm={6} md={3} xl={3} >
+        <Card>
+            <CardActionArea className={classes.area}>
+                <CardMedia 
+                    className={`${classes.media}`}
+                    image="http://localhost:4001/img/CZ75-AUTO/index.webp"
+                />
+
+                <CardContent>
+                    <Typography variant="h6">CZ75-AUTO</Typography>
+                    <Typography variant="body2" component="h2">300 bucks</Typography>
+                </CardContent>
+
+            </CardActionArea>
+            <CardActions>
+                <Button className={classes.bt} size="small">Open</Button>
+            </CardActions>
+        </Card>
+    </Grid>
+}
+
+export default Case
--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid, Paper } from '@material-ui/core';
 import { makeStyles } from "@material-ui/styles";
+import Case from './Case/Case';
 
 const useStyles = makeStyles({
     container: {
@@ -13,8 +14,7 @@ const useStyles = makeStyles({
 
     },
     center: {
-        border: "1px solid black",
-        height: 200
+        
     }
 });
 
@@ -22,17 +22,17 @@ const Content = () => {
     const classes = useStyles();
 
     return <Grid className={classes.container} container>
-        <Grid className={classes.side} item xl={2}  md={1} sm={1} xs={false}>
-
-        </Grid>
+        <Grid className={classes.side} item xl={2}  md={1} sm={1} xs={false}></Grid>{/*sidebar*/}
 
         <Grid className={classes.center} item xl={8}  md={10} sm={10} xs={12}>
-
+            <Case />
+            <Case />
+            <Case />
+            <Case />
+            <Case />
         </Grid>
 
-        <Grid className={classes.side} item xl={2} md={1} sm={1} xs={false}>
-
-        </Grid>
+        <Grid className={classes.side} item xl={2} md={1} sm={1} xs={false}></Grid>{/*sidebar*/}
     </Grid>
 }
 
